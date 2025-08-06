782203eb992ba57c4c7befc953960c2268d4321c
aziel13 <avatarofwill13@gmail.com>
2025-08-06 18:41:45 -0400

    basic working screen and game loop code added

---
+++ main.py
@@ -0,0 +1,26 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+import constants
+from constants import *
+
+pygame.init()
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: 720 {SCREEN_HEIGHT}")
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+if __name__ == "__main__":
+    main()
