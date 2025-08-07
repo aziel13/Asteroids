1bd15a6d60136453fac5c73565d51f21a0febcf3
aziel13 <avatarofwill13@gmail.com>
2025-08-06 21:08:24 -0400

    Add player rendering and input.

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import pygame
 import constants
 from constants import *
 
+from player import Player
+
 pygame.init()
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,11 +29,22 @@ def main():
 
     while True:
         screen.fill((0, 0, 0))
+
+        for playerx in player.containers[0]:
+            playerx.update(dt)
+
+        for drawablex in player.containers[1]:
+            drawablex.draw(screen)
+
         pygame.display.flip()
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
